6bd6a1a5b5fb5e2be3a74e0f53643aa1daf50afb
Alexander <ashengolts87@mail.ru>
2024-11-14 18:36:45 +0900

    add js

---
+++ js/scrollToTop.js
@@ -0,0 +1,14 @@
+//находми элемент
+const scrollButton = document.querySelector('#scrollToTopBt')
+
+window.addEventListener('scroll', function () {
+
+    //если пользователь проскролили больше чем на высоту экрана тогда мы показываем кнопку
+    if (window.pageYOffset > window.innerHeight) {
+        scrollButton.classList.add('top-link--visible')
+    } else {
+        scrollButton.classList.remove('top-link--visible')
+    }
+    console.log(scrollButton)
+
+})
